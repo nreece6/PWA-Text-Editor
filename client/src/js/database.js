c9db504635128aa23227ee760c9b2ad3b4f6b1ce
nreece6 <reece.nick24@gmail.com>
2023-07-15 14:45:18 -0500

    database and button logic

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,39 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// Logic for a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+  console.log('PUT to db')
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+  const contactDb = await openDB('jate', 1)
+
+  const newTransaction = contactDb.transaction('jate', 'readwrite')
+
+  const store = newTransaction.objectStore('jate')
+
+  const request = store.put({ id: 1, value: content })
+
+  const result = await request
+
+  console.log('Data saved to the database', result)
+};
+
+// Logic for a method that gets all the content from the database
+export const getDb = async () => {
+  console.log('GET from db')
+
+  const contactDb = await openDB('jate', 1)
+
+  const newTransaction = contactDb.transaction('jate', 'readonly')
+
+  const store = newTransaction.objectStore('jate')
+
+  const request = store.getAll()
+
+  const result = await request
+
+  console.log('result.value', result)
+  return result.value
+};
 
 initdb();
